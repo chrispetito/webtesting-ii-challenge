72f453527a56eaac344b4028dd9c8d3314f2a67b
chrispetito <christopherpetito@gmail.com>
2019-06-18 12:19:47 -0700

    add styling to count container

--- baseball-app/src/components/Display.js
+++ baseball-app/src/components/Display.js
@@ -2,9 +2,16 @@ import React from "react";
 
 export default function Display(props) {
   return (
-    <div>
-      <h1>{props.count.balls}</h1>
-      <h1>{props.count.strikes}</h1>
+    <div className="count-box">
+      <div>
+          <h2 className='ball-header'>Balls</h2>
+        <h1 className='count-number'>{props.count.balls}</h1>
+      </div>
+
+      <div>
+          <h2 className='strike-header'>Strikes</h2>
+        <h1 className='count-number'>{props.count.strikes}</h1>
+      </div>
     </div>
   );
 }
