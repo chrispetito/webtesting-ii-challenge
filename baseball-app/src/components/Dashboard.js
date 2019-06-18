d4cd70c3e16098efc8e783e55a19fa66bd29aa8d
chrispetito <christopherpetito@gmail.com>
2019-06-18 11:46:42 -0700

    add functions for balls and strikes

--- baseball-app/src/components/Dashboard.js
+++ baseball-app/src/components/Dashboard.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default class Dashboard extends React.Component {
-    state = {
+  state = {
+    balls: 0,
+    strikes: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <h1>Baseball Counter</h1>
+          {this.state.balls}
+          {this.state.strikes}
+          <button onClick={this.ball}>Ball</button>
+          <button onClick={this.strike}>Strike</button>
+          <button>Foul</button>
+          <button>Hit</button>
+        </form>
+      </div>
+    );
+  }
+
+  ball = event => {
+    event.preventDefault();
+    if (this.state.balls >= 3) {
+      this.setState({
         balls: 0,
         strikes: 0
+      });
+    } else {
+      this.setState({
+        balls: this.state.balls + 1
+      });
     }
+  };
 
-    render() {
-        return(
-            <div>
-                <form>
-                    <h1>Hello</h1>
-                    <button>Strike</button>
-                    <button>Ball</button>
-                    <button>Foul</button>
-                    <button>Hit</button>
-                </form>
-            </div>
-        )
+  strike = event => {
+    event.preventDefault();
+    if (this.state.strikes >= 2) {
+      this.setState({
+        strikes: 0,
+        balls: 0
+      });
+    } else {
+      this.setState({
+        strikes: this.state.strikes + 1
+      });
     }
+  };
 }
